4c2175f9ec64d81e7e2e9452c427cde70e58f3ea
root <adrirosgon@gmail.com>
2025-01-20 10:12:07 +0100

    add locks

--- src/es/etg/psp/calculadora/Controller/Calculadora.java
+++ src/es/etg/psp/calculadora/Controller/Calculadora.java
@@ -1,5 +1,10 @@
 package es.etg.psp.calculadora.Controller;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Calculadora implements Calculable{
-    
+    private final ReadWriteLock lock=new ReentrantReadWriteLock();
+    private final Lock w=lock.writeLock();
 }
